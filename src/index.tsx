b6597dd66cf75f00e1b374e167c569cad1a89a0b
Michael Bianco <iloveitaly@gmail.com>
2022-11-08 12:29:25 -0700

    Add a command to show the current focus status
    This command shows the current focus status, including the current schedule,
    whether a pause is active, and whether an override is active.
    
    It also allows the user to pause the current focus session, override the current
    focus session, and configure focus schedules.

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,210 @@
-import { ActionPanel, Detail, List, Action } from "@raycast/api";
+import fetch from "node-fetch";
+import { homedir } from "os";
+import { join } from "path";
 
-export default function Command() {
-  return (
-    <List>
+import { ActionPanel, List, Action, popToRoot, showHUD, Icon } from "@raycast/api";
+import { useFetch } from "@raycast/utils";
+
+export interface FocusStatus {
+  pause: FocusSchedule;
+  override: FocusSchedule;
+  schedule: FocusSchedule;
+}
+
+export interface FocusSchedule {
+  name?: string;
+  until?: number;
+}
+
+const SERVER_PORT = 9029;
+
+function baseURL() {
+  return `http://localhost:${SERVER_PORT}`;
+}
+
+function configPath() {
+  return join(homedir(), ".config/focus/");
+}
+
+function SetOverride(props: { [name: string]: string }) {
+  async function submitOverride(overrideName: string, minutes: number) {
+    const until = Math.round(new Date().getTime() / 1000) + minutes * 60;
+    const result = await fetch(`${baseURL()}/override`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name: overrideName,
+        until: until,
+      }),
+    });
+
+    const jsonResult = await result.json();
+
+    showHUD(`Using schedule '${overrideName}' for ${minutes} minutes`);
+    popToRoot({ clearSearchBar: true });
+  }
+
+  const overrideOptions = [15, 30, 45, 60].map((minutes) => {
+    return (
       <List.Item
-        icon="list-icon.png"
-        title="Greeting"
+        key={minutes}
+        title={`${minutes} minutes`}
         actions={
           <ActionPanel>
-            <Action.Push title="Show Details" target={<Detail markdown="# Hey! 👋" />} />
+            <Action onAction={() => submitOverride(props.name, minutes)} title="Pause" />
           </ActionPanel>
         }
       />
-    </List>
-  );
+    );
+  });
+
+  return <List>{overrideOptions}</List>;
+}
+
+function ChooseOverride() {
+  const { data, isLoading } = useFetch(`${baseURL()}/configurations`);
+
+  const configurationItems = (configurationNames: string[]) =>
+    configurationNames.map((name) => {
+      return (
+        <List.Item
+          key={name}
+          title={name}
+          actions={
+            <ActionPanel>
+              <Action.Push title="Set Override" target={<SetOverride name={name} />} />
+            </ActionPanel>
+          }
+        />
+      );
+    });
+
+  return <List isLoading={isLoading}>{!isLoading && configurationItems(data as string[])}</List>;
+}
+
+function Pause() {
+  async function submitPause(minutes: number) {
+    const until = Math.round(new Date().getTime() / 1000) + minutes * 60;
+
+    const result = await fetch(`${baseURL()}/pause`, {
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        until: until,
+      }),
+      method: "POST",
+    });
+
+    const parsedJson = await result.json();
+
+    if (parsedJson.status == "error") {
+      console.error("Error pausing Focus: ", parsedJson.message);
+    }
+
+    showHUD("Focus schedule paused for " + minutes + " minutes");
+    popToRoot({ clearSearchBar: true });
+  }
+
+  const pauseOptions = [5, 10, 15, 30].map((minutes) => {
+    return (
+      <List.Item
+        key={minutes}
+        title={`${minutes} minutes`}
+        actions={
+          <ActionPanel>
+            <Action onAction={() => submitPause(minutes)} title="Pause" />
+          </ActionPanel>
+        }
+      />
+    );
+  });
+
+  return <List>{pauseOptions}</List>;
+}
+
+function timestampToHoursMinutes(timestamp: number): string {
+  const until = new Date(timestamp * 1000);
+  return until.toLocaleTimeString("en-US", { hour: "2-digit", minute: "2-digit" });
+}
+
+export default function Command() {
+  const { data, isLoading } = useFetch<FocusStatus>(baseURL() + "/status");
+
+  const renderActions = () => {
+    const hasScheduledFocus = data?.schedule?.until !== null;
+    const hasOverrideEnabled = data?.override?.until !== null;
+    const isPaused = data?.pause?.until !== null;
+
+    let summaryItem: React.ReactNode;
+
+    if (isPaused) {
+      summaryItem = (
+        <List.Item
+          icon={Icon.PauseFilled}
+          title="Focus is paused"
+          subtitle={`paused until ${timestampToHoursMinutes(data.pause.until)}`}
+        />
+      );
+    } else if (hasOverrideEnabled) {
+      summaryItem = (
+        <List.Item
+          icon={Icon.Wand}
+          title={`Focusing using '${data?.override?.name}'`}
+          subtitle={`overriding until ${timestampToHoursMinutes(data.override.until)}`}
+        />
+      );
+    } else if (hasScheduledFocus) {
+      summaryItem = (
+        <List.Item
+          icon={Icon.Calendar}
+          title={`Using schedule '${data?.schedule?.name}'`}
+          subtitle={`overriding until ${timestampToHoursMinutes(data.schedule.until)}`}
+        />
+      );
+    } else {
+      summaryItem = <List.Item icon={Icon.Info} title="No focus schedule is active" />;
+    }
+
+    const isAbleToPause = hasScheduledFocus || hasOverrideEnabled;
+
+    return (
+      <>
+        {summaryItem}
+        {isAbleToPause && (
+          <List.Item
+            icon={Icon.Pause}
+            title="Pause"
+            subtitle="pause focus session"
+            actions={
+              <ActionPanel>
+                <Action.Push title="Show Options" target={<Pause />} />
+              </ActionPanel>
+            }
+          />
+        )}
+        <List.Item
+          icon={Icon.Pencil}
+          title="Override"
+          subtitle="schedule a temporary override"
+          actions={
+            <ActionPanel>
+              <Action.Push title="Schedule Override" target={<ChooseOverride />} />
+            </ActionPanel>
+          }
+        />
+        <List.Item
+          icon={Icon.SaveDocument}
+          title="Configure"
+          subtitle="configure focus schedules"
+          actions={
+            <ActionPanel>
+              <Action.ShowInFinder title="Open Configuration" path={configPath()} />
+              <Action.Push title="Schedule Override" target={<ChooseOverride />} />
+            </ActionPanel>
+          }
+        />
+      </>
+    );
+  };
+
+  return <List isLoading={isLoading}>{!isLoading ? renderActions() : null}</List>;
 }
